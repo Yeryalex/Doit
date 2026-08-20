f7b5dbf6865e98cabaccf3764c5a773a4f995167
Yery Rodriguez <128627025+Yeryalex@users.noreply.github.com>
2026-08-20 11:27:38 +0200

    Create input  section  to add each task description and title

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import "./style.css"
+// import arrowUp from "./icons/arrow-up.svg"
 import { message } from "./home.js"
 
+const form = document.querySelector("#task-form");
 const title = document.querySelector("#title");
 const description = document.querySelector("#description");
 const dueDate = document.querySelector("#dueDate");
 const priority = document.querySelector("#priority");
 const buttonInput = document.querySelector(".button-input");
 
-buttonInput.addEventListener("click", (e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault();
+    console.log(e)
     console.log(title.value);
     console.log(description.value);
     console.log(dueDate.value);
