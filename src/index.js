ef1b9c6bcc7c11a352ee4e183b2f451a9e9592e3
Yery Rodriguez <128627025+Yeryalex@users.noreply.github.com>
2026-08-20 09:45:38 +0200

    Access input ellements through a click event

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import "./style.css"
 import { message } from "./home.js"
 
-console.log(message);
+const title = document.querySelector("#title");
+const description = document.querySelector("#description");
+const dueDate = document.querySelector("#dueDate");
+const priority = document.querySelector("#priority");
+const buttonInput = document.querySelector(".button-input");
+
+buttonInput.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(title.value);
+    console.log(description.value);
+    console.log(dueDate.value);
+    console.log(priority.value);
+});
