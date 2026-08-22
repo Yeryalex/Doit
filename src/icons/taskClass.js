dcf6fa5fac22ba3241623fbbb0de5ef529fb02e0
Yery Rodriguez <128627025+Yeryalex@users.noreply.github.com>
2026-08-22 11:26:16 +0200

    Create an array for the objects tasks

--- src/icons/taskClass.js
+++ src/icons/taskClass.js
@@ -1,9 +1,13 @@
+import {format} from "date-fns";
+
+
 export class taskInfo {
  
     constructor(title, description, dueDate, priority) {
+
         this.title = title;
         this.description = description;
-        this.dueDate = dueDate;
+        this.dueDate = format( dueDate.value ? new Date(dueDate.value) : new Date(), "MMM dd yyyy");;
         this.priority = priority;
         this.id = self.crypto.randomUUID();
     }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./style.css"
 import { taskGenerator } from "./taskGenerator.js"
-import {format} from "date-fns"
 import { taskInfo } from "./icons/taskClass.js";
+// import {format} from "date-fns";
 
 const form = document.querySelector("#task-form");
 const title = document.querySelector("#title");
@@ -10,19 +10,29 @@ const dueDate = document.querySelector("#dueDate");
 const priority = document.querySelector("#priority");
 // const buttonArrow = document.querySelector(".button-arrow");
 const taskContainer = document.querySelector(".main-task-container");
+// const dates = format( dueDate.value ? new Date(dueDate.value) : new Date(), "MMM dd yyyy");
 
-const dates = format( dueDate.value ? new Date(dueDate.value) : new Date(), "MMM dd yyyy");
 
+const arrayTasks = [];
 
 form.addEventListener("submit", (e) => {
     
     e.preventDefault();
-    const date = format( dueDate.value ? new Date(dueDate.value) : new Date(), "MMM dd yyyy")
-    const task = taskGenerator(title.value, description.value, date, priority.value);
-    taskContainer.appendChild(task);
 
-    const test = new taskInfo(title.value, description.value, dates, priority.value);
+    arrayTasks.push(new taskInfo(title.value, description.value, dueDate.value, priority.value));
 
-console.log(test)
-console.log(test.getTitle())
+
+    arrayTasks.forEach((e) => {
+        const task = taskGenerator(e.title, e.description, e.dueDate, e.priority);
+        taskContainer.appendChild(task);
+    });
+
+    const checkboxButton = document.querySelector(".checkbox-button");
+
+    checkboxButton.addEventListener("click", () => {
+        const svgContainer= document.querySelector(".svg-container");
+        checkboxButton.style.backgroundColor = "gray";
+        svgContainer.classList.add("svg-style");
+    });
+    console.log(arrayTasks)
 });
